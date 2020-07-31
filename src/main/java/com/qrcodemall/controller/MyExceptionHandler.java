33cc7cc57a43427d05db1d5f9309c06c9a9e3dff
PPeony <1916008936@qq.com>
2020-07-31 11:38:01 +0800

    add GlobalExceptionHandler

---
+++ src/main/java/com/qrcodemall/controller/MyExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.qrcodemall.controller;
+
+import com.qrcodemall.util.Result;
+import io.swagger.annotations.Api;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: Peony
+ * @Date: 2020/7/31 11:32
+ */
+@RestController
+@Api(value = "处理服务器异常，返回code500")
+public class MyExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e, HttpServletRequest req) {
+        System.out.println("common.NewBeeMallExceptionHandler.handleException");
+        Result result = new Result();
+        result.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        result.setMessage("服务器异常");
+        return result;
+        //检查请求是否为ajax, 如果是 ajax 请求则返回 Result json串, 如果不是 ajax 请求则返回 error 视图
+//        String contentTypeHeader = req.getHeader("Content-Type");
+//        String acceptHeader = req.getHeader("Accept");
+//        String xRequestedWith = req.getHeader("X-Requested-With");
+//        if ((contentTypeHeader != null && contentTypeHeader.contains("application/json"))
+//                || (acceptHeader != null && acceptHeader.contains("application/json"))
+//                || "XMLHttpRequest".equalsIgnoreCase(xRequestedWith)) {
+//            return result;
+//        } else {
+//            ModelAndView modelAndView = new ModelAndView();
+//            modelAndView.addObject("message", e.getMessage());
+//            modelAndView.addObject("url", req.getRequestURL());
+//            modelAndView.addObject("stackTrace", e.getStackTrace());
+//            modelAndView.addObject("author", "十三");
+//            modelAndView.addObject("ltd", "新峰商城");
+//            modelAndView.setViewName("error/error");
+//            return modelAndView;
+//        }
+    }
+}
