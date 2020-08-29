8317debc84444d34c934ad4ac0f76f8e823d331a
PPeony <1916008936@qq.com>
2020-08-29 18:00:58 +0800

    update

--- src/main/java/com/qrcodemall/util/Result.java
+++ src/main/java/com/qrcodemall/util/Result.java
@@ -32,7 +32,7 @@ public class Result<T> implements Serializable {
 
     public static Result badUserParams(Integer r) {
         Result result = new Result();
-        result.setCode(HttpStatus.OK.value());
+        result.setCode(HttpStatus.BAD_REQUEST.value());
         switch (r) {
             case -1:result.setMessage("没有父级代理名字");break;
             case -2:result.setMessage("名字重复");break;
